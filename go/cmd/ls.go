e7af16704840a18be42fe03b9aa15153e2dfaf03
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2025-12-12 14:30:09 -0700

    ls now works in go

--- go/cmd/ls.go
+++ go/cmd/ls.go
@@ -1,28 +1,52 @@
 /*
 Copyright © 2025 Harrison Jansen van Beek <harrison@jansenvanbeek.me>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
-var path *string
+var dir *string
+var shouldSort, reverse, all, files *bool
+
 // lsCmd represents the ls command
 var lsCmd = &cobra.Command{
 	Use:   "ls",
 	Short: "list all entries in directory",
 	Long: `This function mimics the functionality of the unix ls command. **Add more detail here.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ls called")
+		entries, err := os.ReadDir(*dir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if *reverse {
+			sortFiles(&entries)
+		}
+		for _, entry := range entries {
+			if !*all && strings.HasPrefix(entry.Name(), "."){
+				continue
+			}
+			fmt.Println(entry.Name())
+
+		}
+
 	},
 }
 
 func init() {
 
-	path = rootCmd.PersistentFlags().StringP("directory", "d", ".", "set a directory to read all entries in")
+	dir = lsCmd.PersistentFlags().StringP("directory", "d", ".", "set a directory to read all entries in")
+	shouldSort = lsCmd.PersistentFlags().BoolP("sort", "s", true, "Sort output alphabetically in ascending order")
+	reverse = lsCmd.PersistentFlags().BoolP("reverse", "r", false, "Sort output alphabetically in descending order")
+	all = lsCmd.PersistentFlags().BoolP("all", "a", false, "show hidden files in output")
+	files = lsCmd.PersistentFlags().BoolP("files", "f", true, "show only regular files in output")
+
 	rootCmd.AddCommand(lsCmd)
 
 	// Here you will define your flags and configuration settings.
@@ -34,4 +58,16 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// lsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func sortFiles(entries *[]os.DirEntry) {
+	if entries == nil || len(*entries) == 0 {
+        return
+    }
+    s := *entries
+    sort.SliceStable(s, func(i, j int) bool {
+            return s[i].Name() > s[j].Name()
+    })
+    *entries = s
+
 }
